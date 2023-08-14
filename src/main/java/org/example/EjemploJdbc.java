5c5f6956e995ce30a203fbf58f9b2d97bbd08515
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-14 12:00:37 +0200

    inital commit

---
+++ src/main/java/org/example/EjemploJdbc.java
@@ -0,0 +1,37 @@
+package org.example;
+
+
+import java.sql.*;
+
+
+public class EjemploJdbc {
+    public static void main(String[] args) {
+
+        String url ="jdbc:mysql://localhost:3306/java_curso?serverTimeZone=europe/madrid";
+
+        String usuario = "root";
+        String password = "";
+
+        try (Connection conn = DriverManager.getConnection(url, usuario, password);
+             Statement stmt = conn.createStatement();
+             ResultSet resultado = stmt.executeQuery("select * from productos");){
+
+            while(resultado.next()){
+
+                System.out.print("id : "+resultado.getInt("id"));
+                System.out.print(" | ");
+                System.out.print("nombre : "+resultado.getString("nombre"));
+                System.out.print(" | ");
+                System.out.print("precio : "+resultado.getInt("precio"));
+                System.out.println(" | ");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+   }
+
+}
